415e3bf53d159b34d9fc71f095c21fa38e859445
Khem chand <khemchand5487@gmail.com>
2021-12-27 17:12:23 +0530

    added iterative approach

--- postorderTraversal.cpp
+++ postorderTraversal.cpp
@@ -1,4 +1,4 @@
-//postorder traversal of a binary tree using recursion in c++
+//postorder traversal of a binary tree in c++
 
 #include <bits/stdc++.h>
 
@@ -26,6 +26,85 @@ void postorder(Node *root)
     cout<<root->data<<" "; //printing root node
 }
 
+/*
+    Time Complexity of below approch: O(N)
+    Time Complexity of below approch: O(2N)
+*/
+
+//iterativ approach using 2 stacks
+void postorder_itr1(Node* root)
+{
+    if(root==NULL) return;
+        
+    stack<Node*> st1;
+    stack<int> st2;
+    st1.push(root);
+    
+    while(!st1.empty())
+    {
+        root = st1.top();
+        st1.pop();
+        st2.push(root->data);
+        if(root->left) st1.push(root->left);
+        if(root->right) st1.push(root->right);
+    }
+    while(!st2.empty())
+    {
+        cout<<st2.top()<<" ";
+        st2.pop();
+    }
+    cout<<endl;
+}
+
+/*
+    Time Complexity of below approch: O(2N)
+    Time Complexity of below approch: O(N)
+*/
+
+//iterativ approach using 1 stacks
+void postorder_itr2(Node* root)
+{
+    vector<int> res;
+    if(root==NULL) return;
+    
+    stack<Node*> st;
+    Node* curr = root, *temp;
+    while(curr || !st.empty())
+    {
+        if(curr!=NULL)
+        {
+            st.push(curr);
+            curr = curr->left;
+        }
+        else
+        {
+            temp = st.top()->right;
+            if(temp==NULL)
+            {
+                temp = st.top();
+                st.pop();
+                res.push_back(temp->data);
+                while(!st.empty() && temp == st.top()->right)
+                {
+                    temp = st.top();
+                    st.pop();
+                    res.push_back(temp->data);
+                }
+            }
+            else
+            {
+                curr=temp;
+            }
+        }
+    }
+        
+    for(int i: res)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+
 int main()
 {
     //creating a tree
@@ -41,6 +120,13 @@ int main()
     root->right->left = new Node(10);
     //END
     
+    //iterativ approach using 2 stacks
+    postorder_itr1(root);
+    
+    //iterativ approach using 1 stacks
+    postorder_itr2(root);
+    
+    //recursive approach
     postorder(root);
 }
 
