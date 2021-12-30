0ab8f154550fa417d50e7c3512342fb4d0bcd2bf
Khem chand <khemchand5487@gmail.com>
2021-12-30 14:53:59 +0530

    Update checkHeightBalanced.cpp

--- checkHeightBalanced.cpp
+++ checkHeightBalanced.cpp
@@ -24,9 +24,11 @@ int check(Node *root)
     if(root == NULL) return 0;
     
     int lh = check(root->left);
+    if(lh==-1) return -1;
     int rh = check(root->right);
+    if(rh==-1) return -1;
     
-    if((lh == -1 || rh==-1) || (abs(lh-rh) >1)) return -1;
+    if((abs(lh-rh) >1)) return -1;
     
     return 1+max(lh, rh);
 }
