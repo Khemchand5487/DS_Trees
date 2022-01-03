fa59cd35589209632192e8399524c2059f5cad5c
Khem chand <khemchand5487@gmail.com>
2022-01-03 17:23:30 +0530

    Create leftView_BT.cpp

---
+++ leftView_BT.cpp
@@ -0,0 +1,67 @@
+//Left view of a binary tree in c++
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *right, *left;
+    
+    Node(int val)
+    {
+        data = val;
+        left=right=NULL;
+    }
+};
+
+void util(Node* root, int level, vector<int>& res)
+{
+    if(root==NULL) return;
+    
+    if(level == res.size()) res.push_back(root->data);
+    
+    util(root->left, level+1, res);
+    util(root->right, level+1, res);
+}
+
+//left side view of binary tree 
+void leftSideView(Node* root) {
+    vector<int> res;
+    if(!root) return;
+    util(root,0, res);
+    
+    for(int i:res)
+    {
+        cout<<i<<" ";
+    }
+}
+
+int main()
+{
+    //creating a tree
+    struct Node *root = new Node(8);
+    
+    root->left = new Node(5);
+    root->right = new Node(9);
+    
+    root->left->left = new Node(1);
+    root->left->right = new Node(3);
+    
+    root->right->right = new Node(2);
+    root->right->left = new Node(10);
+    //END
+    
+    //left view of tree
+    leftSideView(root);
+}
+
+/*
+The above code represent a binary tree like below:
+                         8
+                      /    \
+                     5      9
+                    / \    / \
+                  1    3  10  2
+*/
